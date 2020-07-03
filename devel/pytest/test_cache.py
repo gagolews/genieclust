3c4400a725618309c587bbaa5d46c44f34decbf2
gagolews <m.gagolewski@gmail.com>
2020-07-03 13:29:47 +1000

    pytest upd

---
+++ devel/pytest/test_cache.py
@@ -0,0 +1,4 @@
+# TODO: test the appropriateness of the "cached" version of Genie and Genie+Approx
+# TODO: change params and compare with ran from scratch
+# TODO: assure run-times are better
+# TODO: mention this in a notebook/tutorial
