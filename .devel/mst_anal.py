e7eefc4adbb4ed8657e97d2d977ea4ecb3c3eff9
gagolews <m.gagolewski@gmail.com>
2025-02-01 22:29:12 +0100

    more

--- .devel/mst_anal.py
+++ .devel/mst_anal.py
@@ -6,7 +6,7 @@ import clustbench
 
 import os.path
 data_path = os.path.join("~", "Projects", "clustering-data-v1")
-b = clustbench.load_dataset("wut", "x2", path=data_path)
+b = clustbench.load_dataset("sipu", "pathbased", path=data_path)
 X = b.data
 #labels = b.labels[0]
 
@@ -33,17 +33,20 @@ def visit(v, e, c):  # v->w  where mst_e[e,:]={v,w}
         if mst_e[e2, 0] != v and mst_e[e2, 1] != v:
             tot += visit(w, e2, c)
     mst_s[e, iv] = tot
-    #mst_s[e, 1-iv] = n-tot
+    mst_s[e, 1-iv] = 0
     return tot
 
 
 
-
+min_cluster_size = n/20
+# TODO: store mst_s in each iteration
+# TODO: restart from the vertices adjacent to the skipped edge
+skiplist = [293, 271]
 mst_s = np.zeros((n-1, 2), dtype=int)
 labels = np.zeros(n, dtype=int)
 mst_labels = np.zeros(n-1, dtype=int)
-mst_labels[112] = -1  # skiplist   # TODO
-mst_labels[88] = -1
+for s in skiplist:
+    mst_labels[s] = -1  # skiplist
 c = 0
 while True:
     v = np.argmin(labels)
@@ -58,21 +61,18 @@ counts = np.bincount(labels)
 for i in range(n-1):
     j = int(mst_s[i, 1] == 0)
     mst_s[i, j] = counts[mst_labels[i]]-mst_s[i, 1-j]
-
-
-min_cluster_size = 20
 min_mst_s = np.min(mst_s, axis=1)
 op = np.argsort(mst_w)[::-1]
 #(mst_w[op])[min_mst_s[op]>min_cluster_size]
 which_cut = op[np.nonzero(min_mst_s[op]>min_cluster_size)]
 print(which_cut)
 #
-plt.cld()
+plt.clf()
 #
 ax1 = plt.subplot(2, 2, 1)
 ax1.plot(mst_w[op])
 ax2 = ax1.twinx()
-ax2.plot(np.arange(n-1), min_mst_s[op], c='orange')
+ax2.plot(np.arange(n-1), min_mst_s[op], c='orange', alpha=0.3)
 for i in which_cut:
     plt.text(op[i], min_mst_s[i], i, ha='center')
 #
@@ -89,5 +89,25 @@ for i in range(n-1):
         "%d (%d)" % (i, min(mst_s[i, 0], mst_s[i, 1]))
     )
 #
-plt.subplot(2, 2, 3)
+ax1 = plt.subplot(2, 2, 3)
+ax2 = ax1.twinx()
+last = 0
+for i in range(1, c+1):
+    _w = mst_w[mst_labels==i]
+    _o = np.argsort(_w)[::-1]
+    ax1.plot(
+        np.arange(last, last+counts[i]-1),
+        _w[_o],
+        color=genieclust.plots.col[i]
+    )
+    ax2.plot(
+        np.arange(last, last+counts[i]-1),
+        min_mst_s[mst_labels==i][_o],
+        color=genieclust.plots.col[i],
+        alpha=0.2
+    )
+    last += counts[i] - 1
 
+
+# DBSCAN is non-adaptive - cannot detect clusters of different densities well
+plt.violinplot([ mst_w[mst_labels==i] for i in range(1, c+1) ])
