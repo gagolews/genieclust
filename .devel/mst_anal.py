8fe7ba3b66a05ab9b115fa9df8ede597f72c2ff0
gagolews <m.gagolewski@gmail.com>
2025-02-07 17:08:18 +0100

    more

--- .devel/mst_anal.py
+++ .devel/mst_anal.py
@@ -16,17 +16,18 @@ import os.path
 
 
 examples = [
+    ["sipu", "aggregation", [785, 784, 786, 783]],
+    ["sipu", "spiral", [310, 309]],
+    ["sipu", "compound", [397, 396, 328, 347]],
     ["other", "hdbscan", [2051, 2047, 2034, 1975, 1942]],
     ["graves", "parabolic", [956]],
     ["fcps", "wingnut", [1014]],
     ["fcps", "engytime", [3597]],
     ["sipu", "pathbased", [293, 271]],
-    ["sipu", "aggregation", [785, 784, 786, 783, 360]],
     ["wut", "z2", [894, 895, 893, 889]],
     ["wut", "z2", [897, 895, 893, 892, 683]],
     ["wut", "z2", [897, 895, 893]],
     ["wut", "labirynth", [3544, 3543, 3541, 3533]],
-    ["sipu", "spiral", [310, 309]],
 ]
 
 example = examples[0]
@@ -41,7 +42,7 @@ SKIPEDGE = np.iinfo(int).min
 
 n = X.shape[0]
 min_cluster_size = n/(len(skiplist)+1)/10
-noise_size = 5
+noise_size = 3
 
 mst = genieclust.internal.mst_from_distance(X, "euclidean")
 mst_w, mst_e = mst
