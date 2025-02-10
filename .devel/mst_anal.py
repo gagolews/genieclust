c299be3e83e4c9cb93eede49691c146d1daec664
gagolews <m.gagolewski@gmail.com>
2025-02-10 17:00:10 +0100

    more

--- .devel/mst_anal.py
+++ .devel/mst_anal.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import clustbench
 import os.path
-
+import scipy.spatial
 
 # from sklearn.datasets import make_blobs
 # X, labels = make_blobs(
@@ -16,16 +16,17 @@ import os.path
 
 
 examples = [
-    ["sipu", "aggregation", [785, 784, 786, 783]],
+    ["fcps", "wingnut", [1014]],
     ["sipu", "spiral", [310, 309]],
+    ["wut", "z2", [895]],
+    ["sipu", "aggregation", [785, 784, 786, 783]],
+    ["graves", "parabolic", [956]],
+    ["sipu", "pathbased", [293, 271]],
     ["sipu", "compound", [397, 396, 328, 347]],
     ["other", "hdbscan", [2051, 2047, 2034, 1975, 1942]],
-    ["graves", "parabolic", [956]],
-    ["fcps", "wingnut", [1014]],
     ["fcps", "engytime", [3597]],
-    ["sipu", "pathbased", [293, 271]],
-    ["wut", "z2", [894, 895, 893, 889]],
     ["wut", "z2", [897, 895, 893, 892, 683]],
+    ["wut", "z2", [894, 895, 893, 889]],
     ["wut", "z2", [897, 895, 893]],
     ["wut", "labirynth", [3544, 3543, 3541, 3533]],
 ]
@@ -41,8 +42,9 @@ SKIPEDGE = np.iinfo(int).min
 
 
 n = X.shape[0]
-min_cluster_size = n/(len(skiplist)+1)/10
+min_cluster_size = n/(len(skiplist)+1)/4
 noise_size = 3
+gini_threshold = 0.5
 
 mst = genieclust.internal.mst_from_distance(X, "euclidean")
 mst_w, mst_e = mst
@@ -80,11 +82,12 @@ for s in skiplist:
 #
 # preprocess – noise points:
 c = 0
-v = 0  # anything
-for e in adj_list[v]:
-    visit(v, e, c)
+# v = 0  # anything
+# for e in adj_list[v]:
+#     visit(v, e, c)
 #
 # mark clusters
+c = 0
 for v in range(n):
     if labels[v] == -1:
         c += 1
@@ -99,7 +102,22 @@ for i in range(n-1):
 min_mst_s = np.min(mst_s, axis=1)
 
 
+nn_k = 5
+kd = scipy.spatial.KDTree(X)
+nn_w, nn_e = kd.query(X, nn_k+1)
+nn_w = np.array(nn_w)[:, 1:]  # exclude self
+nn_e = np.array(nn_e)[:, 1:]
 
+nn_threshold = np.mean(nn_w[:, -1])
+nn_w[:, -1] > nn_threshold
+
+plt.clf()
+genieclust.plots.plot_scatter(X)
+plt.axis("equal")
+for i in range(nn_k):
+    genieclust.plots.plot_segments(np.c_[np.arange(n), nn_e[:,i]], X)
+
+raise Exception("")
 
 #
 plt.clf()
@@ -112,11 +130,21 @@ op = op[mst_labels[op]>0]
 ax1.plot(mst_w[op])
 ax2 = ax1.twinx()
 ax2.plot(np.arange(len(op)), min_mst_s[op], c='orange', alpha=0.3)
-# mark potential cut edges
+# Variant 1) mark candidate cut edges based on cut sizes
 which_cut = np.flatnonzero(min_mst_s[op]>min_cluster_size)
-print(op[which_cut])
 for i in which_cut:
-    plt.text(i, min_mst_s[op[i]], op[i], ha='center')
+   plt.text(i, min_mst_s[op[i]], op[i], ha='center', color=genieclust.plots.col[mst_labels[op[i]]-1])
+# Variant 2) mark candidate cut edges based on Gini indices of cluster sizes
+# which_cut = []
+# for i in range(len(op)):
+#     assert mst_labels[op[i]] > 0
+#     #g = genieclust.inequality.gini_index(np.r_[np.delete(counts, [0, mst_labels[op[i]]]), mst_s[op[i], :]])  # doesn't work well
+#     g = genieclust.inequality.gini_index(mst_s[op[i], :])
+#     if g < gini_threshold and min_mst_s[op[i]] > min_cluster_size:
+#         plt.text(i, min_mst_s[op[i]], op[i], ha="center")
+#        which_cut.append(i)
+#
+print(op[which_cut])
 #
 # MST edges per cluster
 ax1 = plt.subplot(3, 2, 3)
@@ -166,10 +194,13 @@ s = np.where(a<b, 1.0 - a/b, b/a - 1.0)
 o1 = np.argsort(s)[::-1]
 o2 = np.argsort(l[o1], kind='stable')
 plt.bar(np.arange(len(s)), s[o1][o2], width=1.0, color=np.array(genieclust.plots.col)[l[o1]-1][o2])
-plt.axhline(np.mean(s), color='gray')
+treelhouette_score = np.mean(s)
+print(treelhouette_score)
+plt.axhline(treelhouette_score, color='gray')
 #
 # MST
 plt.subplot(3, 2, (2, 6))
+labels[nn_w[:, -1] > nn_threshold] = 0
 genieclust.plots.plot_scatter(X, labels=labels-1)
 genieclust.plots.plot_segments(mst_e, X, style="b-", alpha=0.1)
 #genieclust.plots.plot_segments(mst_e[min_mst_s>min_cluster_size,:], X, alpha=0.5)
@@ -188,4 +219,7 @@ for i in range(n-1):
 # DBSCAN is non-adaptive - cannot detect clusters of different densities well
 #plt.violinplot([ mst_w[mst_labels==i] for i in range(1, c+1) ])
 
+
+# r = np.median(mst_w[mst_labels == 3])
+# def visit2(v, e, r):
 
