ab7fd363a5706e9e600ad79bd8c094e9f601d061
gagolews <m.gagolewski@gmail.com>
2025-06-04 20:02:39 +0200

    pico_tree copyright info

--- src/pico_tree/kd_tree.hpp
+++ src/pico_tree/kd_tree.hpp
@@ -1,3 +1,33 @@
+/*
+ * PicoTree: a C++ header only library for fast nearest neighbor
+ * and range searches using a KdTree.
+ *
+ * <https://github.com/Jaybro/pico_tree>
+ *
+ * Version 1.0.0 (c5f719837df9707ee12d94cb0108aa0c34bfe96f)
+ *
+ * Copyright (c) 2025 Jonathan Broere
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
 #pragma once
 
 #include "pico_tree/internal/box.hpp"
